4407da53913725dba4220bb9129c137a6dafe104
miyata09x0084 <59190800+miyata09x0084@users.noreply.github.com>
2020-12-21 17:23:05 +0900

    OSC MIXモジュールを操作するパラメータ群を管理するクラスの宣言

--- SimpleSynth/Source/DSP/SimpleSynthParameters.h
+++ SimpleSynth/Source/DSP/SimpleSynthParameters.h
@@ -22,3 +22,22 @@ public:
     virtual void saveParameters(juce::XmlElement& xml) = 0;
     virtual void loadParameters(juce::XmlElement& xml) = 0;
 };
+
+class OscillatorParameters : public SynthParametersBase
+{
+public:
+    juce::AudioParameterFloat* SineWaveLevel;
+    juce::AudioParameterFloat* SawWaveLevel;
+    juce::AudioParameterFloat* TriWaveLevel;
+    juce::AudioParameterFloat* SquareWaveLevel;
+    juce::AudioParameterFloat* NoiseLevel;
+    
+    OscillatorParameters(juce::AudioParameterFloat* sineWaveLevel, juce::AudioParameterFloat* sawWaveLevel, juce::AudioParameterFloat* triWaveLevel, juce::AudioParameterFloat* squareWaveLevel, juce::AudioParameterFloat* noiseLevel);
+    
+    virtual void addAllParameters(juce::AudioProcessor& processor) override;
+    virtual void saveParameters(juce::XmlElement& xml) override;
+    virtual void loadParameters(juce::XmlElement& xml) override;
+    
+private:
+    OscillatorParameters() {};
+};
