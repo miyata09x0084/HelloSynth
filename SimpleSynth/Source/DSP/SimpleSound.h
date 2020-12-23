89f7393007c2d9ceaa5de93c8f353614f1d8be59
miyata09x0084 <59190800+miyata09x0084@users.noreply.github.com>
2020-12-23 16:55:44 +0900

    MIDIノートとチャンネル番号の一覧を保持する変数、それらの判定結果を返す関数を定義するクラス

--- SimpleSynth/Source/DSP/SimpleSound.h
+++ SimpleSynth/Source/DSP/SimpleSound.h
@@ -9,3 +9,21 @@
 */
 
 #pragma once
+
+#include "../JuceLibraryCode/JuceHeader.h"
+
+class SimpleSound : public juce::SynthesiserSound
+{
+public:
+    SimpleSound(const juce::BigInteger& notes, const juce::BigInteger& channels);
+
+    virtual ~SimpleSound();
+
+    virtual bool appliesToNote(int midiNoteNumber) override;
+    virtual bool appliesToChannel(int midiChannel) override;
+
+private:
+    juce::BigInteger midiNotes;
+    juce::BigInteger midiChannels;
+
+};
