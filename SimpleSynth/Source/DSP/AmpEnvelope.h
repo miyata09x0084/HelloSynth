abeefb66d468aac36b722a4f7c2108311555bcdb
miyata09x0084 <59190800+miyata09x0084@users.noreply.github.com>
2020-12-23 16:15:20 +0900

    エンベロープのパラメータの状態を取得・変更するAmpEnvelopeクラスの宣言

--- SimpleSynth/Source/DSP/AmpEnvelope.h
+++ SimpleSynth/Source/DSP/AmpEnvelope.h
@@ -9,3 +9,37 @@
 */
 
 #pragma once
+
+class AmpEnvelope
+{
+public:
+     enum class AMPENV_STATE
+     {
+         ATTACK = 0,
+         DECAY,
+         SUSTAIN,
+         RELEASE,
+         WAIT,
+     };
+    
+    AmpEnvelope(float attackTime, float decayTime, float sustain, float releaseTime);
+    ~AmpEnvelope();
+    
+    AMPENV_STATE getState();
+    float getValue();
+    void setParameters(float attackTime, float decayTime, float sustain, float releaseTime);
+    void attackStart();
+    void releaseStart();
+    void releaseEnd();
+    bool isHolding();
+    bool isReleasing();
+    void cycle(float sampleRate);
+    
+private:
+    AmpEnvelope();
+
+    AMPENV_STATE _ampState;
+    float _attackTime, _decayTime, _sustainValue, _releaseTime;
+    float _value, _valueOnReleaseStart;
+    float _sampleRate;
+};
