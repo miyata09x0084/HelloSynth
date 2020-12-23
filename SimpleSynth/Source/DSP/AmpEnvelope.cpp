642429ecf6ed249ede56aca17e150818b846715c
miyata09x0084 <59190800+miyata09x0084@users.noreply.github.com>
2020-12-23 16:21:47 +0900

    初期化指定子にて引数で渡された各パラメータの初期値をメンバ変数に代入

--- SimpleSynth/Source/DSP/AmpEnvelope.cpp
+++ SimpleSynth/Source/DSP/AmpEnvelope.cpp
@@ -18,3 +18,29 @@ namespace
     const float DECAY_MIN = 0.01f;
     const float RELEASE_MIN = 0.01f;
 }
+
+AmpEnvelope::AmpEnvelope(float attackTime, float decayTime, float sustain, float releaseTime)
+    : _attackTime(attackTime), _decayTime(decayTime), _sustainValue(sustain), _releaseTime(releaseTime)
+    , _sampleRate(0.0f), _value(0.0f), _valueOnReleaseStart(0.0f), _ampState(AMPENV_STATE::WAIT)
+{
+    // 各パラメータの値を定数で記述した最大値・最小値の範囲に収める。
+    if (_attackTime <= ATTACK_MIN) {
+        _attackTime = ATTACK_MIN;
+    }
+
+    if (_decayTime <= DECAY_MIN) {
+        _decayTime = DECAY_MIN;
+    }
+
+    if (_releaseTime <= RELEASE_MIN) {
+        _releaseTime = RELEASE_MIN;
+    }
+
+    if (_sustainValue > AMP_MAX) {
+        _sustainValue = AMP_MAX;
+    }
+
+    if (_sustainValue < AMP_MIN) {
+        _sustainValue = AMP_MIN;
+    }
+}
