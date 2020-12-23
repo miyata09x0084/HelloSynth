8423397b03750e2fdac60634c678e6ae7a845427
miyata09x0084 <59190800+miyata09x0084@users.noreply.github.com>
2020-12-23 16:19:00 +0900

    無名名前空間に定数宣言を記述

--- SimpleSynth/Source/DSP/AmpEnvelope.cpp
+++ SimpleSynth/Source/DSP/AmpEnvelope.cpp
@@ -9,3 +9,12 @@
 */
 
 #include "AmpEnvelope.h"
+
+namespace
+{
+    const float AMP_MAX = 1.0f;
+    const float AMP_MIN = 0.0f;
+    const float ATTACK_MIN = 0.01f;
+    const float DECAY_MIN = 0.01f;
+    const float RELEASE_MIN = 0.01f;
+}
