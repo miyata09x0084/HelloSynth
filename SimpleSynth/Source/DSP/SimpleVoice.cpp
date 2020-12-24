bc623436fa97fad76739590344ca7a9ad2a5e7f2
miyata09x0084 <59190800+miyata09x0084@users.noreply.github.com>
2020-12-24 15:29:37 +0900

    基底クラスで宣言された純粋仮想関数をオーバーライド宣言

--- SimpleSynth/Source/DSP/SimpleVoice.cpp
+++ SimpleSynth/Source/DSP/SimpleVoice.cpp
@@ -21,4 +21,11 @@ class SimpleVoice : public juce::SynthesiserVoice
     SimpleVoice(OscillatorParameters* oscParams, LfoParameters* lfoParams, AmpEnvelopeParameters* ampEnvParams, juce::AudioParameterBool* velocitySenseParam);
 
     virtual ~SimpleVoice();
+    
+    virtual bool canPlaySound(juce::SynthesiserSound* sound) override;
+    virtual void startNote(int midiNoteNumber, float velocity, juce::SynthesiserSound* sound, int currentPitchWheelPosition) override;
+    virtual void stopNote(float velocity, bool allowTailOff) override;
+    virtual void pitchWheelMoved(int newPitchWheelValue) override;
+    virtual void controllerMoved(int controllerNumber, int newControllerValue) override;
+    virtual void renderNextBlock(juce::AudioBuffer<float>& outputBuffer, int startSample, int numSamples) override;
 };
