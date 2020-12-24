1d11e06d7eafd9f09174f426a62c90f95f9f802a
miyata09x0084 <59190800+miyata09x0084@users.noreply.github.com>
2020-12-24 15:42:36 +0900

    ソースコードをSampleVoice.cppからSampleVoice.hに移動

--- SimpleSynth/Source/DSP/SimpleVoice.cpp
+++ SimpleSynth/Source/DSP/SimpleVoice.cpp
@@ -9,38 +9,3 @@
 */
 
 #include "SimpleVoice.h"
-
-#include "../JuceLibraryCode/JuceHeader.h"
-#include "SimpleSynthParameters.h"
-#include "Waveforms.h"
-#include "AmpEnvelope.h"
-#include "SimpleSound.h"
-
-class SimpleVoice : public juce::SynthesiserVoice
-{
-    SimpleVoice(OscillatorParameters* oscParams, LfoParameters* lfoParams, AmpEnvelopeParameters* ampEnvParams, juce::AudioParameterBool* velocitySenseParam);
-
-    virtual ~SimpleVoice();
-    
-    virtual bool canPlaySound(juce::SynthesiserSound* sound) override;
-    virtual void startNote(int midiNoteNumber, float velocity, juce::SynthesiserSound* sound, int currentPitchWheelPosition) override;
-    virtual void stopNote(float velocity, bool allowTailOff) override;
-    virtual void pitchWheelMoved(int newPitchWheelValue) override;
-    virtual void controllerMoved(int controllerNumber, int newControllerValue) override;
-    virtual void renderNextBlock(juce::AudioBuffer<float>& outputBuffer, int startSample, int numSamples) override;
-    
-private:
-    float calcModulationFactor(float angle);
-
-    float currentAngle, lfoAngle, angleDelta;
-    float level, lastLevel, levelDiff;
-    float pitchBend;
-
-    Waveforms waveForms;
-    AmpEnvelope ampEnv;
-
-    OscillatorParameters* _oscParamsPtr;
-    LfoParameters* _lfoParamsPtr;
-    AmpEnvelopeParameters* _ampEnvParamsPtr;
-    juce::AudioParameterBool* _velocitySenseParamPtr;
-};
--- SimpleSynth/Source/DSP/SimpleVoice.h
+++ SimpleSynth/Source/DSP/SimpleVoice.h
@@ -9,3 +9,41 @@
 */
 
 #pragma once
+
+#pragma once
+
+#include "../JuceLibraryCode/JuceHeader.h"
+#include "SimpleSynthParameters.h"
+#include "Waveforms.h"
+#include "AmpEnvelope.h"
+#include "SimpleSound.h"
+
+class SimpleVoice : public SynthesiserVoice
+{
+public:
+    SimpleVoice(OscillatorParameters* oscParams, LfoParameters* lfoParams, AmpEnvelopeParameters* ampEnvParams, AudioParameterBool* velocitySenseParam);
+
+    virtual ~SimpleVoice();
+
+    virtual bool canPlaySound(SynthesiserSound* sound) override;
+    virtual void startNote(int midiNoteNumber, float velocity, SynthesiserSound* sound, int currentPitchWheelPosition) override;
+    virtual void stopNote(float velocity, bool allowTailOff) override;
+    virtual void pitchWheelMoved(int newPitchWheelValue) override;
+    virtual void controllerMoved(int controllerNumber, int newControllerValue) override;
+    virtual void renderNextBlock(AudioBuffer<float>& outputBuffer, int startSample, int numSamples) override;
+
+private:
+    float calcModulationFactor(float angle);
+
+    float currentAngle, lfoAngle, angleDelta;
+    float level, lastLevel, levelDiff;
+    float pitchBend;
+
+    Waveforms waveForms;
+    AmpEnvelope ampEnv;
+
+    OscillatorParameters* _oscParamsPtr;
+    LfoParameters* _lfoParamsPtr;
+    AmpEnvelopeParameters* _ampEnvParamsPtr;
+    AudioParameterBool* _velocitySenseParamPtr;
+};
