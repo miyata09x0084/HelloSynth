ce299578e4c14bee357b4363f49ad700af143c79
miyata09x0084 <59190800+miyata09x0084@users.noreply.github.com>
2020-12-24 16:21:09 +0900

    引数付きコンストラクタ

--- SimpleSynth/Source/DSP/SimpleVoice.cpp
+++ SimpleSynth/Source/DSP/SimpleVoice.cpp
@@ -11,7 +11,20 @@
 #include "SimpleVoice.h"
 
 namespace {
-    const float HALF_PI = MathConstants<float>::halfPi;
-    const float ONE_PI = MathConstants<float>::pi;
-    const float TWO_PI = MathConstants<float>::twoPi;
+    const float HALF_PI = juce::MathConstants<float>::halfPi;
+    const float ONE_PI = juce::MathConstants<float>::pi;
+    const float TWO_PI = juce::MathConstants<float>::twoPi;
 }
+
+SimpleVoice::SimpleVoice(OscillatorParameters* oscParams, LfoParameters* lfoParams, AmpEnvelopeParameters* ampEnvParams, juce::AudioParameterBool* velocitySenseParam)
+    : _oscParamsPtr(oscParams)
+    , _lfoParamsPtr(lfoParams)
+    , _ampEnvParamsPtr(ampEnvParams)
+    , _velocitySenseParamPtr(velocitySenseParam)
+    , ampEnv(ampEnvParams->Attack->get(), ampEnvParams->Decay->get(), ampEnvParams->Sustain->get(), ampEnvParams->Release->get())
+    , currentAngle(0.0f), lfoAngle(0.0f), angleDelta(0.0f)
+    , level(0.0f), lastLevel(0.0f), levelDiff(0.0f)
+    , pitchBend(0.0f)
+{}
+
+
