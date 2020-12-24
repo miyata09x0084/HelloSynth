14be8a07a7ef22fa10a9d7b696212c345f145b7d
miyata09x0084 <59190800+miyata09x0084@users.noreply.github.com>
2020-12-24 16:24:40 +0900

    Synthesiserクラスから呼び出されるノートONのトリガー

--- SimpleSynth/Source/DSP/SimpleVoice.cpp
+++ SimpleSynth/Source/DSP/SimpleVoice.cpp
@@ -34,3 +34,34 @@ bool SimpleVoice::canPlaySound(juce::SynthesiserSound* sound)
 {
     return dynamic_cast<const SimpleSound*> (sound) != nullptr;
 }
+
+void SimpleVoice::startNote(int midiNoteNumber, float velocity, juce::SynthesiserSound* sound, int currentPitchWheelPosition)
+{
+    DBG("[StartNote] NoteNumber: " + juce::String(midiNoteNumber) + ", Velocity: " + juce::String(velocity));
+
+    if (SimpleSound* soundForPlay = dynamic_cast<SimpleSound*> (sound))
+    {
+        if (_velocitySenseParamPtr->get())
+        {
+            if (velocity <= 0.01f) {
+                velocity = 0.01f;
+            }
+            level = velocity * 0.4f;
+        }
+        else
+        {
+            level = 0.8f;
+        }
+
+        levelDiff = level - lastLevel;
+
+        pitchBend = ((float)currentPitchWheelPosition - 8192.0f) / 8192.0f;
+
+        float cyclesPerSecond = (float)juce::MidiMessage::getMidiNoteInHertz(midiNoteNumber);
+        float cyclesPerSample = (float)cyclesPerSecond / (float)getSampleRate();
+        angleDelta = cyclesPerSample * TWO_PI;
+
+        ampEnv.attackStart();
+    }
+}
+
