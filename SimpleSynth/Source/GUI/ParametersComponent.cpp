9ea9fb76366242c53a85131ff17b7a889ef313bd
miyata09x0084 <59190800+miyata09x0084@users.noreply.github.com>
2021-01-04 16:29:00 +0900

    ファイル内で共通して使用する定数を無名名前空間にまとめる

--- SimpleSynth/Source/GUI/ParametersComponent.cpp
+++ SimpleSynth/Source/GUI/ParametersComponent.cpp
@@ -9,3 +9,11 @@
 */
 
 #include "ParametersComponent.h"
+
+namespace {
+    const juce::Colour PANEL_COLOUR = juce::Colour(36, 36, 36);
+    const float PANEL_NAME_FONT_SIZE = 24.0f;
+    const float PARAM_LABEL_FONT_SIZE = 16.0f;
+    const int PANEL_NAME_HEIGHT = 42;
+    const int LOCAL_MARGIN = 2;
+}
